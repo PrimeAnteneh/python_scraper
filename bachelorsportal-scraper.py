12331620bace921d4b43f6cf8484f504391d1356
Anteneh_Tsegaye <73240768+PrimeAnteneh@users.noreply.github.com>
2025-06-24 22:23:49 +0300

    Update bachelorsportal-scraper.py

--- bachelorsportal-scraper.py
+++ bachelorsportal-scraper.py
@@ -26,11 +26,21 @@ class BachelorsPortalSeleniumScraper:
         chrome_options = Options()
         if headless:
             chrome_options.add_argument("--headless=new")
+        #chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-infobars")
+        chrome_options.add_argument("--remote-debugging-port=9222")
         chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+
+        #chrome_options.add_argument("--disable-gpu")
+        #chrome_options.add_argument("--no-sandbox")
+        #chrome_options.add_argument("--window-size=1920,1080")
+        #chrome_options.add_argument("--disable-dev-shm-usage")
+        #chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
 
         service = Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
